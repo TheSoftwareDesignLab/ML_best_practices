3d8ed7f19db85ad908d9c22f85ab0b477ff2c0e0
AndreaBayona <ma.bayona@uniandes.edu.co>
2022-07-01 11:00:53 -0500

    add page padding

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -7,6 +7,8 @@ import {TemplateTable} from "../../components/tables/TemplateTable";
 
 import {TABLE_1_COLUMNS, TABLE_1} from "../../data/table1";
 
+import {Container} from "./styles";
+
 type Props = {
   data: any;
 }
@@ -15,18 +17,18 @@ export const MainPage: React.FC<Props> = ({data}) => {
   const [openTax, setOpenTax] = React.useState(false);
 
   return (
-    <>
+    <Container>
       {!openTax ? <>
-          <h1>What are the Machine Learning best practices reported by practitioners on Stack Exchange?</h1>
-          <Typography variant="h5">Taxonomy</Typography>
+          <Typography variant="h3" align="center" margin={10}>What are the Machine Learning best practices reported by practitioners on Stack Exchange?</Typography>
+          <Typography variant="h4">Taxonomy</Typography>
           <Button onClick={() => setOpenTax(true)}>Open chart</Button>
           <br/>
           <br/>
-          <Typography variant="h5">Complementary tables of practices</Typography>
+          <Typography variant="h4">Complementary tables of practices</Typography>
           <TemplateTable data={TABLE_1} columns={TABLE_1_COLUMNS} tableHeight={500}/>
         </>
         : <TaxPage data={data} onBackClick={() => setOpenTax(false)}/>}
-    </>
+    </Container>
   )
 
 }
