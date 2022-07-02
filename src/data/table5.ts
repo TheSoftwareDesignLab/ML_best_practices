246d3c0499dee81896194f399870feccc5c0dedf
Anamaria I Mojica-Hanke <ai.mojica10@uniandes.edu.co>
2022-07-02 18:41:05 -0500

    Data cleaning table until DCL15

---
+++ src/data/table5.ts
@@ -0,0 +1,212 @@
+export const TABLE_5 = [
+  {
+    "id": "Dcl12",
+    "Phase Category": "Wrangling",
+    "Subcategory": "Impute missing data",
+    "Description": "If an imputation technique is used, the same technique should be used in the training, validation and testing sets.",
+    "Number of experts that validated the practice": 4,
+    "STE Post(s)": "https://stats.stackexchange.com/questions/503677",
+    "External URL(S) associated to the post": "-"
+  },
+  {
+    "id": "Dcl13",
+    "Phase Category": "Wrangling",
+    "Subcategory": "Balance data",
+    "Description": "When having an unbalanced dataset, balancing the datashould be done only in the training dataset.",
+    "Number of experts that validated the practice": 4,
+    "STE Post(s)": "https://stackoverflow.com/questions/51064462\nhttps://datascience.stackexchange.com/questions/82012",
+    "External URL(S) associated to the post": "https://stackoverflow.com/questions/48805063/h2o-balance-classes-cross-validation/48810493#48810493\nhttps://imbalanced-learn.org/stable/",
+  },
+  {
+    "id": "Dcl14",
+    "Phase Category": "Wrangling",
+    "Subcategory": "Balance data",
+    "Description": "Oversampling should be done only after the whole dataset is split into train, validation and test sets.",
+    "Number of experts that validated the practice": 4,
+    "STE Post(s)": "https://stackoverflow.com/questions/51064462",
+    "External URL(S) associated to the post": "https://stackoverflow.com/questions/48805063/h2o-balance-classes-cross-validation/48810493#48810493"
+  },
+  {
+    "id": "Dcl15",
+    "Phase Category": "Wrangling",
+    "Subcategory": "Balance data",
+    "Description": "When undersampling data, the samples that are being selected should be randomly sampled.",
+    "Number of experts that validated the practice": 4,
+    "STE Post(s)": "https://stackoverflow.com/questions/46178141",
+    "External URL(S) associated to the post": "https://en.wikipedia.org/wiki/Oversampling_and_undersampling_in_data_analysis"
+  },
+  {
+    "id": "Dcl16",
+    "Phase Category": "Wrangling",
+    "Subcategory": "",
+    "Description": "",
+    "Number of experts that validated the practice": 4,
+    "STE Post(s)": "",
+    "External URL(S) associated to the post": ""
+  },
+  {
+    "id": "Dcl17",
+    "Phase Category": "Wrangling",
+    "Subcategory": "",
+    "Description": "",
+    "Number of experts that validated the practice": 4,
+    "STE Post(s)": "",
+    "External URL(S) associated to the post": ""
+  },
+  {
+    "id": "Dcl18",
+    "Phase Category": "Wrangling",
+    "Subcategory": "",
+    "Description": "",
+    "Number of experts that validated the practice": 4,
+    "STE Post(s)": "",
+    "External URL(S) associated to the post": ""
+  },
+  {
+    "id": "Dcl19",
+    "Phase Category": "Wrangling",
+    "Subcategory": "",
+    "Description": "",
+    "Number of experts that validated the practice": 4,
+    "STE Post(s)": "",
+    "External URL(S) associated to the post": ""
+  },
+  {
+    "id": "Dcl20",
+    "Phase Category": "Wrangling",
+    "Subcategory": "",
+    "Description": "",
+    "Number of experts that validated the practice": 4,
+    "STE Post(s)": "",
+    "External URL(S) associated to the post": ""
+  },
+  {
+    "id": "Dcl21",
+    "Phase Category": "Wrangling",
+    "Subcategory": "",
+    "Description": "",
+    "Number of experts that validated the practice": 4,
+    "STE Post(s)": "",
+    "External URL(S) associated to the post": ""
+  },
+  {
+    "id": "Dcl22",
+    "Phase Category": "Wrangling",
+    "Subcategory": "",
+    "Description": "",
+    "Number of experts that validated the practice": 4,
+    "STE Post(s)": "",
+    "External URL(S) associated to the post": ""
+  },
+  {
+    "id": "Dcl23",
+    "Phase Category": "Wrangling",
+    "Subcategory": "",
+    "Description": "",
+    "Number of experts that validated the practice": 4,
+    "STE Post(s)": "",
+    "External URL(S) associated to the post": ""
+  },
+  {
+    "id": "Dcl24",
+    "Phase Category": "Wrangling",
+    "Subcategory": "",
+    "Description": "",
+    "Number of experts that validated the practice": 4,
+    "STE Post(s)": "",
+    "External URL(S) associated to the post": ""
+  },
+  {
+    "id": "Dcl25",
+    "Phase Category": "Wrangling",
+    "Subcategory": "",
+    "Description": "",
+    "Number of experts that validated the practice": 4,
+    "STE Post(s)": "",
+    "External URL(S) associated to the post": ""
+  },
+  {
+    "id": "Dcl26",
+    "Phase Category": "Wrangling",
+    "Subcategory": "",
+    "Description": "",
+    "Number of experts that validated the practice": 4,
+    "STE Post(s)": "",
+    "External URL(S) associated to the post": ""
+  },
+  {
+    "id": "Dcl27",
+    "Phase Category": "Wrangling",
+    "Subcategory": "",
+    "Description": "",
+    "Number of experts that validated the practice": 4,
+    "STE Post(s)": "",
+    "External URL(S) associated to the post": ""
+  },
+  {
+    "id": "Dcl28",
+    "Phase Category": "Wrangling",
+    "Subcategory": "",
+    "Description": "",
+    "Number of experts that validated the practice": 4,
+    "STE Post(s)": "",
+    "External URL(S) associated to the post": ""
+  },
+  {
+    "id": "Dcl29",
+    "Phase Category": "Wrangling",
+    "Subcategory": "",
+    "Description": "",
+    "Number of experts that validated the practice": 4,
+    "STE Post(s)": "",
+    "External URL(S) associated to the post": ""
+  },
+  {
+    "id": "Dcl30",
+    "Phase Category": "Wrangling",
+    "Subcategory": "",
+    "Description": "",
+    "Number of experts that validated the practice": 4,
+    "STE Post(s)": "",
+    "External URL(S) associated to the post": ""
+  },
+  {
+    "id": "Dcl31",
+    "Phase Category": "Wrangling",
+    "Subcategory": "",
+    "Description": "",
+    "Number of experts that validated the practice": 4,
+    "STE Post(s)": "",
+    "External URL(S) associated to the post": ""
+  },
+  {
+    "id": "Dcl32",
+    "Phase Category": "Wrangling",
+    "Subcategory": "",
+    "Description": "",
+    "Number of experts that validated the practice": 4,
+    "STE Post(s)": "",
+    "External URL(S) associated to the post": ""
+  },
+  {
+    "id": "Dcl33",
+    "Phase Category": "Wrangling",
+    "Subcategory": "",
+    "Description": "",
+    "Number of experts that validated the practice": 4,
+    "STE Post(s)": "",
+    "External URL(S) associated to the post": ""
+  },
+];
+
+export const TABLE_5_COLUMNS = [
+  { field: 'id', headerName: "ID", width: 160 },
+  { field: 'Phase Category', headerName: "Phase Category", width: 150 },
+  { field: 'Subcategory', headerName: "Subcategory", width: 210 },
+  { field: 'Description', headerName: "Description", width: 300 },
+  { field: 'Number of experts that validated the practice', headerName: "Number of experts that" +
+      " validated the practice", width: 200, type: 'number', },
+  { field: 'STE Post(s)', headerName: "STE Post(s)", width: 250 },
+  { field: 'External URL(S) associated to the post', headerName: "External URL(S) associated to" +
+      " the post", width: 300 },
+];
