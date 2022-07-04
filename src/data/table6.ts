0a9e2a458d224a9c41d33cdf480e63239df7b7aa
Anamaria I Mojica-Hanke <ai.mojica10@uniandes.edu.co>
2022-07-04 16:30:28 -0500

    extra terminology

--- src/data/table6.ts
+++ src/data/table6.ts
@@ -10,7 +10,9 @@ export const TABLE_6 = [
     "STE Post(s)": "https://datascience.stackexchange.com/questions/12344",
     "External URL(S) in post(s)": "https://www.mturk.com",
     "STE Post(s)-link": "DTSC/12344",
-    "External URL(S) in post(s)-link": "Amazon's Mechanical Turk"
+    "External URL(S) in post(s)-link": "Amazon's Mechanical Turk",
+    "References to practice terminology": "https://www.mturk.com/",
+    "References to practice terminology-link": "For example: AWS Mechanical Turk",
   },
   {
     "id": "Dl2",
@@ -20,28 +22,36 @@ export const TABLE_6 = [
     "Number of experts that validated the practice": 4,
     "STE Post(s)": "https://stackoverflow.com/questions/40476041",
     "External URL(S) in post(s)": "-",
-    "STE Post(s)-link": "STO/40476041"
+    "STE Post(s)-link": "STO/40476041",
+    "References to practice terminology": "-",
   }
 ];
 
 export const TABLE_6_COLUMNS = [
-  {field: 'id', headerName: "ID", width: 70, sortable: false},
-  {field: 'Phase Category', headerName: "Phase Category", width: 200, sortable: false},
-  {field: 'Subcategory', headerName: "Subcategory", width: 160, sortable: false},
+  {field: 'id', headerName: "ID", width: 80, sortable: false},
+  {field: 'Phase Category', headerName: "Phase Category", width: 100, sortable: false},
+  {field: 'Subcategory', headerName: "Subcategory", width: 170, sortable: false},
   {field: 'Description', headerName: "Description", width: 300, sortable: false},
+  {
+    field: 'References to practice terminology',
+    headerName: "References to practice terminology",
+    width: 290,
+    sortable: false,
+    renderCell: renderUrlCell,
+  },
   {
     field: 'STE Post(s)',
     headerName: "STE Post(s)",
-    width: 200,
-    renderCell: renderUrlCell,
-    sortable: false
+    width: 190,
+    sortable: false,
+    renderCell: renderUrlCell
   },
   {
     field: 'External URL(S) in post(s)',
     headerName: "External URL(S) in post(s)",
-    width: 200,
-    renderCell: renderUrlCell,
+    width: 250,
     sortable: false,
+    renderCell: renderUrlCell,
   },
   {
     field: 'STE Post(s)-link',
@@ -52,10 +62,15 @@ export const TABLE_6_COLUMNS = [
     hide: true,
   },
   {
+    field: 'References to practice terminology-link',
+    hide: true,
+  },
+  {
     field: 'Number of experts that validated the practice',
     headerName: "Number of experts that validated the practice",
     width: 200,
     type: 'number',
-    sortable: false
+    sortable: false,
+    hide: true,
   },
 ];
